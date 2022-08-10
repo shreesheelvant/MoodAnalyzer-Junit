32ef586b1e9efda6163d2ec7f04fc7f2ca78c94e
unknown <suntanabhagyashree@gmail.com>
2022-08-10 16:47:06 +0530

    Thrown Exception for invalid mood

--- src/test/java/com/bridgelabz/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -8,27 +8,53 @@ public class MoodAnalyzerTest {
     @Test
     public void givenMessage_WhenSad_ShouldReturnSad() {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in sad mood");
-        String mood = moodAnalyzer.analyzeMood();
-        Assert.assertEquals("SAD", mood);
+        String mood;
+        try {
+            mood = moodAnalyzer.analyzeMood();
+            Assert.assertEquals("SAD", mood);
+        } catch (MoodAnalyzerException e) {
+            System.out.println(e);
+        }
     }
-
 
     @Test
     public void givenMessage_WhenNotSad_ShouldReturnHappy() {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in any mood");
-        String mood = moodAnalyzer.analyzeMood();
-        Assert.assertEquals("HAPPY", mood);
-
+        String mood;
+        try {
+            mood = moodAnalyzer.analyzeMood();
+            Assert.assertEquals("HAPPY", mood);
+        } catch (MoodAnalyzerException e) {
+            System.out.println(e);
+        }
     }
 
     @Test
-    public void givenMessage_WhenNotSad_ShouldReturnHappy() {
+    public void givenMessage_WhenNull_ShouldReturnHappy() {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer("null");
-        String mood = moodAnalyzer.analyzeMood();
-        Assert.assertEquals("HAPPY", mood);
+        String mood = null;
+        try {
+            mood = moodAnalyzer.analyzeMood();
+            Assert.assertEquals("HAPPY", mood);
+        } catch (MoodAnalyzerException e) {
+            System.out.println(e);
 
+        }
     }
 
+    @Test
+    public void givenMessage_WhenEmpty_ShouldReturnHappy() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("null");
+        String mood;
+        try {
+            mood = moodAnalyzer.analyzeMood();
+            Assert.assertEquals("HAPPY", mood);
+        } catch (MoodAnalyzerException e) {
+            System.out.println(e);
+            System.out.println("Empty mood");
+        }
+    }
+}
 
 
 
